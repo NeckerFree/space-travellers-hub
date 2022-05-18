6bbf713db5dcbb82ed7be15b1a2baec46ac7955a
Elio Cortes <elio.cortes3000@gmail.com>
2022-05-18 14:20:45 -0500

    Create and Display Missions

---
+++ src/react/components/MyProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <section className="profileContainer">
+    <section className="profileSection">
+      <p>My Profile</p>
+    </section>
+  </section>
+);
+export default MyProfile;
