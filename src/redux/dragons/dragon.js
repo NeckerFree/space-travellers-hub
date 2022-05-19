dbf43b6f86cc99ac2466e44793b30ef1547812dc
Godwin Nwachukwu <nwgwin@gmail.com>
2022-05-19 15:29:38 +0100

    refactor dragon reducer

--- src/redux/dragons/dragon.js
+++ src/redux/dragons/dragon.js
@@ -7,23 +7,6 @@ const RECEIVE_DRAGONS = 'spacetraveler/dragons/RECEIVE_DRAGONS';
 // reducer
 export default function dragons(state = [], action) {
   switch (action.type) {
-    // case ADD_BOOK:
-    //   return [...state, {
-    //     id: action.book.id,
-    //     title: action.book.title,
-    //     category: action.book.category,
-    //     author: action.book.author,
-    //     completion: action.book.completion,
-    //   }];
-    // case REMOVE_BOOK:
-    //   return state.filter((book) => book.id !== action.id);
-    // case TOGGLE_BOOK:
-    //   return state.map((book) => {
-    //     if (book.id !== action.id) {
-    //       return book;
-    //     }
-    //     return { ...book, complete: !book.complete };
-    //   });
     case RECEIVE_DRAGONS:
       return action.dragons;
     default:
@@ -38,32 +21,8 @@ function receiveDragons(dragons) {
     dragons,
   };
 }
-// function removeBook(id) {
-//   return {
-//     type: REMOVE_BOOK,
-//     id,
-//   };
-// }
-// function toggleBook(id) {
-//   return {
-//     type: TOGGLE_BOOK,
-//     id,
-//   };
-// }
 
 // Thunk action creators
-// export function handleAddBook(appid, bookobj) {
-//   return (dispatch) => {
-//     dispatch(addBook(bookobj));
-
-//     return createBook(appid, bookobj)
-//       .catch(() => {
-//         showConnectionError();
-//         dispatch(removeBook(bookobj.id));
-//       });
-//   };
-// }
-
 export function handleReceiveDragons(dragons) {
   return (dispatch) => API.getAllDragons(dragons)
     .then((dragons) => {
